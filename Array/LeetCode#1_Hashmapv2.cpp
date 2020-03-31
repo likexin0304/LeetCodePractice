48e1a6352a130ffeeef485afed958ad245f878a1
KEXIN LI <danielleeprince@gmail.com>
2020-03-31 20:51:17 +0800

    Create LeetCode#1_Hashmapv2.cpp

---
+++ Array/LeetCode#1_Hashmapv2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+    
+        for(int i = 0; i < nums.size(); i++)
+        {
+            //um.find(23) == um.end()
+            if( m.find(target - nums[i]) != m.end())
+            {
+                return{m[target - nums[i]],i};
+            }
+            m[nums[i]] = i;
+        }
+        return{};
+
+    
+        
+       
+    }
+};
